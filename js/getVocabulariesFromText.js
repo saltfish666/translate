84c09b629c981dd8d10ceeff7ce6b4a0b26c8bdf
LiBai666 <overwall2016@gmail.com>
2018-04-26 16:13:59 +0800

    用正则表达式提取文本中的单词，并去重

--- js/getVocabulariesFromText.js
+++ js/getVocabulariesFromText.js
@@ -4,7 +4,14 @@ function isVocabularyInLocalStorage(vocabulary){
 
 function getVocabulariesFromText(text){
 
-	var vocabulariesArray = text.trim().split(" ") //不好，不能适应多种变化
+	//var vocabulariesArray = text.trim().split(" ") //不好，不能适应多种变化
+
+	var vocabulariesArray = text.match(/[a-zA-Z]+/g)
+
+	vocabulariesArray = new Set(vocabulariesArray)//去重
+	vocabulariesArray = [...vocabulariesArray]
+
+	console.log(vocabulariesArray)
 
     //next realse 用正则提取字符串
 	//var vocabulariesArray = text.
