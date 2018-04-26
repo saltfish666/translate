7275f1f4e40b4bb9d13481f9a0c84db98b626484
LiBai666 <overwall2016@gmail.com>
2018-04-26 14:00:41 +0800

    使返回的结果为对象格式，将翻译的结果以表格的形式展示

--- js/getTrans.js
+++ js/getTrans.js
@@ -31,8 +31,23 @@ function getTrans(queryArray,successFun){
         success:  function (data) {
             console.log(data);
             console.log(data.trans_result[0].dst)
-            //document.getElementById("showTransResult").value = data.trans_result[0].dst
-             successFun(data.trans_result)
+
+            //data is json data of translate result,such as: 
+            //{"from":"en","to":"zh","trans_result":[{src: "A!powerful!movie!that!demands!attention", dst: "A！强大！电影！那！要求！注意"}]}
+            var trans_result = data.trans_result[0];
+
+            //将源词汇和翻译结果整合成字典（key-value 对象）
+            var srcArray = trans_result["src"].split("!")
+            var dstArray = trans_result["dst"].split("！")//中文 "！"；
+            console.log(srcArray)
+            console.log(dstArray)
+            var trans_result_map = {}
+            for(let i = 0;i< srcArray.length;i++){
+                trans_result_map[srcArray[i]] = dstArray[i]
+            }
+            console.log(trans_result_map)
+
+             successFun(trans_result_map)
         } 
     });
 }
